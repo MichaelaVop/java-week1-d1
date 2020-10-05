5dead6edcbc406c18e566b7ffcd60f7405d42a23
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2020-10-05 21:06:41 +0200

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,21 @@
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+
+        byte myAge = 30;
+
+        int postalCode = 36471;
+
+        System.out.println(myAge);
+
+        System.out.println(postalCode);
+
+        myAge = 31;
+
+        System.out.println(myAge);
+
+
+
+    }
+
+}
